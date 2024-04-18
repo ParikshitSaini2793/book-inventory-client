5b341b5fceff837de3589e0735d568ce402ea8f4
Parikshit Saini <parikshit.sce21@sot.pdpu.ac.in>
2024-04-18 23:29:22 +0530

    ..

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCIQl0weHWkyX7FNmGNCTElUYL9aL0HPMA",
-    authDomain: "online-book-store-78516.firebaseapp.com",
-    projectId: "online-book-store-78516",
-    storageBucket: "online-book-store-78516.appspot.com",
-    messagingSenderId: "282181520275",
-    appId: "1:282181520275:web:0ce28538cea0433ea83666"
-  };
+  apiKey: "AIzaSyDqCy_z_2stE9wMu3SE_C7gunzA8g7tdG4",
+  authDomain: "book-inventory-d599d.firebaseapp.com",
+  projectId: "book-inventory-d599d",
+  storageBucket: "book-inventory-d599d.appspot.com",
+  messagingSenderId: "601811403572",
+  appId: "1:601811403572:web:c33f81872fb402ab0c5b61"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
